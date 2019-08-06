7fe173bd6dc29b5f03c1579f72b9d4712f4a3aa8
yyx0612 <709096733@qq.com>
2019-08-06 21:42:05 +0800

    解决问题

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,13 +6,12 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("再次提交新代码");
-<<<<<<< HEAD
 		System.out.println("王五提交新代码");
 		System.out.println("王五提交新代码");
-=======
+
         System.out.println("第三次提交新代码");
         System.out.println("李四次提交新代码");
         System.out.println("李四次提交新代码");
->>>>>>> ae53738033ca7e876970de4e566a251d54ca41e8
+
     }
 }
